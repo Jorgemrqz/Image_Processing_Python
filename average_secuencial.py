bb2c5520f5bf87c35b20bc050a02ddd34f375910
Erika <evillaq1@est.ups.edu.ec>
2025-11-17 09:29:50 -0500

    feat: función cargar_imagen_gris

--- average_secuencial.py
+++ average_secuencial.py
@@ -1,3 +1,6 @@
+# promedio_secuencial_puro.py
+from PIL import Image
+
 # -------------------------------------------------
 # Generar kernel promedio NxN (todo 1/(n*n))
 # -------------------------------------------------
@@ -7,3 +10,18 @@ def generar_kernel_promedio(n):
     val = 1.0 / (n * n)
     kernel = [[val for _ in range(n)] for _ in range(n)]
     return kernel
+
+# -------------------------------------------------
+# Cargar imagen como matriz 2D (gray[y][x])
+# -------------------------------------------------
+def cargar_imagen_gris(ruta):
+    img = Image.open(ruta).convert("L")
+    w, h = img.size
+    pix = img.load()
+
+    gray = [[0 for _ in range(w)] for _ in range(h)]
+    for y in range(h):
+        for x in range(w):
+            gray[y][x] = pix[x, y]
+
+    return gray, w, h
