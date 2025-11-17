397d75a980263d776a425172207efbae7934f920
Jorgemrqz <166621479+Jorgemrqz@users.noreply.github.com>
2025-11-16 23:31:57 -0500

    feat: crear script base para Sobel en GPU con PyCUDA

---
+++ sobel_pycuda.py
@@ -0,0 +1,16 @@
+# sobel_pycuda.py
+
+import pycuda.autoinit
+import pycuda.driver as drv
+from pycuda.compiler import SourceModule
+import numpy as np
+from PIL import Image
+from time import perf_counter
+
+print("=== Sobel PyCUDA (GPU) ===")
+
+def main():
+    print("Implementación GPU para filtro Sobel")
+
+if __name__ == "__main__":
+    main()
